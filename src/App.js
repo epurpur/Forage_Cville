fb87087021366e579584bce060078cb1c6f2ff03
Erich Purpur <epurpur@gmail.com>
2023-06-30 18:34:13 -0400

    adding blueberry image

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
-import './App.css';
+import React, { useEffect } from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import Map from './components/Map'
 
-import Map from "./components/Map"
+const App = () => {
+  useEffect(() => {
+    const legend = L.control({ position: 'bottomright' });
 
-function App() {
+    legend.onAdd = () => {
+      const div = L.DomUtil.create('div', 'legend');
+
+      const legendItems = [
+        { label: 'Legend Item 11', color: '#ff0000' },
+        { label: 'Legend Item 2', color: '#00ff00' },
+        { label: 'Legend Item 3', color: '#0000ff' },
+      ];
+
+      legendItems.forEach((item) => {
+        const label = document.createElement('span');
+        label.innerHTML = item.label;
+        label.style.backgroundColor = item.color;
+        div.appendChild(label);
+      });
+
+      return div;
+    };
+
+    const map = L.map('map');
+    const tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors',
+    });
+
+    legend.addTo(map);
+    tileLayer.addTo(map);
+
+    return () => {
+      map.remove();
+    };
+  }, []);
+
   return (
-    <main>
-        <h1>My React App</h1>
-        <h2> Test </h2>
-        <div id="mapContainer">
-            <Map />
-        </div>
-    </main>
-
+    <div>
+      <div id="map" style={{ height: '400px' }} />
+      <Map />
+    </div>
   );
-}
+};
 
 export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
 
 const Map = () => {
+    const [map, setMap] = useState(null);
     const [mapID, setMapID] = useState(1)
     const position = [38.0293, -78.4767]; // Initial position of the map marker over Charlottesville
 
+   
+
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false} id='mapContainer'>
+    <MapContainer key={mapID} center={position} zoom={13} scrollWheelZoom={false} id='mapContainer'>
         <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
     </MapContainer>
   );
 };
         
-
-
 
 export default Map;
